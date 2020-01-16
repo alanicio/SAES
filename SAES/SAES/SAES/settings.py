42cedf00af1c6b175ebc74c4bd04ee4c5455713b
root <root@201802239-ranx.praxisit.local>
2020-01-16 14:16:49 -0600

    Layouts basicos creados

--- SAES/SAES/SAES/settings.py
+++ SAES/SAES/SAES/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'SAES.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),
+                (os.path.join(BASE_DIR, 'templates/layouts')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
