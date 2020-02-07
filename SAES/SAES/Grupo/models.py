c75bbba79f0b5015cc9751dea312d422bcdff5a1
root <root@201802239-ranx.praxisit.local>
2020-02-07 12:57:10 -0600

    modelos horarios creado y relacionado

--- SAES/SAES/Grupo/models.py
+++ SAES/SAES/Grupo/models.py
@@ -9,3 +9,13 @@ class GrupoMateria(models.Model):
 	grupo=models.ForeignKey(Grupo,on_delete=models.CASCADE)
 	materia=models.ForeignKey('Materia.Materia',on_delete=models.CASCADE)
 	inscritos=models.PositiveSmallIntegerField()
+
+class Horario(models.Model):
+	lunes=models.CharField(max_length=11)
+	martes=models.CharField(max_length=11)
+	miercoles=models.CharField(max_length=11)
+	jueves=models.CharField(max_length=11)
+	viernes=models.CharField(max_length=11)
+	sabado=models.CharField(max_length=11)
+	domingo=models.CharField(max_length=11)
+	curso=models.OneToOneField(GrupoMateria,on_delete=models.CASCADE,primary_key=True)
