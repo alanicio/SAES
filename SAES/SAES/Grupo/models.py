8f9a13ef13eec632f65ae070198974c1c652c0e5
root <root@201802239-ranx.praxisit.local>
2020-02-10 12:44:39 -0600

    horario y calificaciones creado y correctamente relacionados

--- SAES/SAES/Grupo/models.py
+++ SAES/SAES/Grupo/models.py
@@ -5,11 +5,6 @@ class Grupo(models.Model):
 	turno=models.CharField(max_length=11,blank=True,null=True,)
 	materia=models.ManyToManyField('Materia.Materia', through='GrupoMateria')
 
-class GrupoMateria(models.Model):
-	grupo=models.ForeignKey(Grupo,on_delete=models.CASCADE)
-	materia=models.ForeignKey('Materia.Materia',on_delete=models.CASCADE)
-	inscritos=models.PositiveSmallIntegerField()
-
 class Horario(models.Model):
 	lunes=models.CharField(max_length=11)
 	martes=models.CharField(max_length=11)
@@ -18,4 +13,11 @@ class Horario(models.Model):
 	viernes=models.CharField(max_length=11)
 	sabado=models.CharField(max_length=11)
 	domingo=models.CharField(max_length=11)
-	curso=models.OneToOneField(GrupoMateria,on_delete=models.CASCADE,primary_key=True)
+
+# GrupoMateria es el modelo general de la relacion Grupo-materia
+	# De uso general en reinscripciones
+class GrupoMateria(models.Model):
+	grupo=models.ForeignKey(Grupo,on_delete=models.CASCADE)
+	materia=models.ForeignKey('Materia.Materia',on_delete=models.CASCADE)
+	inscritos=models.PositiveSmallIntegerField()
+	horario=models.OneToOneField(Horario,on_delete=models.CASCADE,primary_key=True)
--- SAES/SAES/Materia/models.py
+++ SAES/SAES/Materia/models.py
@@ -7,11 +7,24 @@ class Materia(models.Model):
 	departamento=models.ForeignKey('Departamento.Departamento',on_delete=models.CASCADE)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
-		
+
+# MateriaInscrita son los datos individuales de los alumnos con su materia
 class MateriaInscrita(models.Model):
 	alumno=models.ForeignKey('Persona.Alumno',on_delete=models.CASCADE)
 	grupo_materia=models.ForeignKey('Grupo.GrupoMateria',on_delete=models.CASCADE)
-	
+	periodo=models.CharField(max_length=7)
+	fecha_inscrita=models.DateTimeField(auto_now_add=True)
+	aprobada=models.BooleanField(null=True)
+	# Si aprobada es:
+	# 	null=Se esta cursando
+	# 	false=Se reprobo
+	#	true=Ya se curso y aprobo
 
-	
-		
+class Calificaciones(models.Model):
+	materia=models.ForeignKey(MateriaInscrita,on_delete=models.CASCADE)
+	primer_parcial=models.PositiveSmallIntegerField()
+	segundo_parcial=models.PositiveSmallIntegerField()
+	tercer_parcial=models.PositiveSmallIntegerField()
+	final=models.PositiveSmallIntegerField()
+	extraordinario=models.PositiveSmallIntegerField(null=True)
+	ets=models.PositiveSmallIntegerField(null=True)
