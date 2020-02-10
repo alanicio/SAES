654dd0ab9bb01d5a19a3a3a6f5becef2752dce18
root <root@201802239-ranx.praxisit.local>
2020-02-10 14:25:39 -0600

    view con index rest

--- SAES/SAES/Persona/views.py
+++ SAES/SAES/Persona/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+from django.core import serializers
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    usuarios=User.objects.all()
+    data=serializers.serialize('json',usuarios)
+    return HttpResponse(data,content_type='application/json')
+
