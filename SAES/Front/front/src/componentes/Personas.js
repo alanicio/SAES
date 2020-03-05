e1a972563f8a4468d4441aef94d98731d77ff38c
root <root@201802239-ranx.praxisit.local>
2020-03-05 14:34:14 -0600

    peticion get con token realizada y respondido correctamente

--- SAES/Front/front/src/componentes/Personas.js
+++ SAES/Front/front/src/componentes/Personas.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 
 class Personas extends React.Component{
+
+	componentDidMount() {
+	    window.addEventListener('load', this.getPersonas);
+	}
+
 	render(){
 		return(
 			<div>
 				<h1>Personas</h1>
 			</div>
 			);
+	}
+
+	getPersonas=()=>{
+		axios.get('http://127.0.0.1:8000/personas/',{
+			headers:{
+				'Authorization':' Token '+localStorage.getItem('token'),
+			}
+		}).then(
+			(response)=>{
+				console.log(response);
+			}
+		).catch();
 	}
 }
 
